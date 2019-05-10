6b628a933d0c769ef92990abdd428343ca7196f4
Soroush Khosravi <sorush.kh68@gmail.com>
2019-05-10 15:59:06 +0100

    There is a form in the application now.

--- app.rb
+++ app.rb
@@ -11,3 +11,15 @@ get '/cats' do
     <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png' />
   </div>"
 end
+
+get '/named' do
+  @name = params["name"]
+  p params
+  erb :named
+end
+
+post "/show-name" do
+  @name = params[:name]
+  @family = params[:family]
+  erb :showFullName
+end
